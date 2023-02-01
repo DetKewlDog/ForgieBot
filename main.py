2b6ac7580f7415bc1141fdc6993df070c9973ce4
DetKewlDog <79573375+DetKewlDog@users.noreply.github.com>
2023-02-01 11:43:07 +0000

    fixed indents

--- main.py
+++ main.py
@@ -4,52 +4,45 @@ import os
 import keep_alive
 
 cogs: list = [
-  "Functions.Bugtracker.bug",
-  "Functions.Bugtracker.bugmod",
-  "Functions.Bugtracker.bugdel",
-  "Functions.Bugtracker.bugclear",
-
-
-  "Functions.Suggestions.suggest",
-  "Functions.Suggestions.suggestmod",
-  "Functions.Suggestions.suggestdel",
-  "Functions.Suggestions.suggestclear",
-
-  "Functions.Feedback.feedback",
-
-  "Functions.Mod.reactionroles",
-  "Functions.Mod.release",
-  "Functions.Mod.clear",
-  "Functions.Mod.clearall"
+    "Functions.Bugtracker.bug", "Functions.Bugtracker.bugmod",
+    "Functions.Bugtracker.bugdel", "Functions.Bugtracker.bugclear",
+    "Functions.Suggestions.suggest", "Functions.Suggestions.suggestmod",
+    "Functions.Suggestions.suggestdel", "Functions.Suggestions.suggestclear",
+    "Functions.Feedback.feedback", "Functions.Mod.reactionroles",
+    "Functions.Mod.release", "Functions.Mod.clear", "Functions.Mod.clearall"
 ]
 
 try:
-  client = Client(token=os.getenv('TOKEN'))
+    client = Client(token=os.getenv('TOKEN'))
 except:
-  os.system('kill 1')
+    os.system('kill 1')
+
 
 @client.event
 async def on_ready():
-  print("Bot is ready!")
-  await client.change_presence(
-    ClientPresence(
-      status=StatusType.ONLINE,
-      activities=[ PresenceActivity( name=BotStatus , type=PresenceActivityType.GAME , created_at=0 ) ],
-      afk=False
-    )
-)
+    print("Bot is ready!")
+    await client.change_presence(
+        ClientPresence(status=StatusType.ONLINE,
+                       activities=[
+                           PresenceActivity(name=BotStatus,
+                                            type=PresenceActivityType.GAME,
+                                            created_at=0)
+                       ],
+                       afk=False))
+
+
 for cog in cogs:
-  try:
-    print(f"Loading cog {cog}")
-    client.load(cog)
-    print(f"Loaded cog {cog}")
-  except Exception as e:
-    exc = "{}: {}".format(type(e).__name__, e)
-    print("Failed to load cog {}\n{}".format(cog, exc))
+    try:
+        print(f"Loading cog {cog}")
+        client.load(cog)
+        print(f"Loaded cog {cog}")
+    except Exception as e:
+        exc = "{}: {}".format(type(e).__name__, e)
+        print("Failed to load cog {}\n{}".format(cog, exc))
 
 keep_alive.keep_alive()
 
 try:
-  client.start()
+    client.start()
 except:
-  os.system('kill 1')
+    os.system('kill 1')
--- suggestion.py
+++ suggestion.py
@@ -2,15 +2,17 @@ from interactions import *
 from settings import *
 from bugreport import *
 
-class Suggestion:
-  def __init__(self, msg, id):
-    self.msg = msg
-    self.id = id
 
-  def __str__(self):
-    return f"{self.msg} {self.id}"
+class Suggestion:
+
+    def __init__(self, msg, id):
+        self.msg = msg
+        self.id = id
+
+    def __str__(self):
+        return f"{self.msg} {self.id}"
 
 
 def str_to_sug(str):
-  msg, id = str.replace('\n', '').split(' ')
-  return Suggestion(msg, id)
+    msg, id = str.replace('\n', '').split(' ')
+    return Suggestion(msg, id)
